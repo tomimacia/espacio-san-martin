db4497e7f9317a0e6ddf6a31443c90f6a3228395
Tomás Macía <tomimacia@gmail.com>
2023-09-21 10:31:10 -0300

    Sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl:'https://www.sanmartinjuancruz.com.ar',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
