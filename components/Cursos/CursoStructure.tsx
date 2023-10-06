5efeba862df8f08f30b6fc58ec7fc2cde3e11e1c
Tomás Macía <tomimacia@gmail.com>
2023-10-06 13:59:45 -0300

    Cursos image metadata

--- components/Cursos/CursoStructure.tsx
+++ components/Cursos/CursoStructure.tsx
@@ -12,6 +12,7 @@ import {
 import Image from "next/image";
 import { Fragment } from "react";
 import { CursoForm } from "./CursosForm";
+import Head from "next/head";
 const BackButton = () => {
   return (
     <Button
@@ -29,47 +30,59 @@ const BackButton = () => {
   );
 };
 const CursoStructure = ({ Curso }: Curso) => {
-  const { title, subtitle, description, img } = Curso;
+  const { title, subtitle, description, route, img } = Curso;
   return (
-    <Flex py={10} gap={5} w="100%" flexDir="column">
-      <Flex flexDir="column" align="center" justify="center" w="100%">
-        <Heading textAlign="center">{title}</Heading>
+    <>
+      <Head>
+        <meta
+          property="og:description"
+          content={`Descubre nuestro curso de ${title}`}
+        />
+        <meta
+          property="og:image"
+          content={`https://www.sanmartinjuancruz.com.ar/Cursos/${route}.jpeg`}
+        />
+      </Head>
+      <Flex py={10} gap={5} w="100%" flexDir="column">
+        <Flex flexDir="column" align="center" justify="center" w="100%">
+          <Heading textAlign="center">{title}</Heading>
+          <BackButton />
+        </Flex>
+        <Divider borderColor="gray.500" w="85%" m="auto" />
+        <Flex display="block" gap={2} p={3} flexDir="column">
+          <Box float="right" p={1}>
+            <Image
+              style={{
+                objectFit: "contain",
+                objectPosition: "center",
+                borderRadius: "10px",
+                border: "1px solid rgba(175, 175, 175, 0.4)",
+                padding: 1,
+              }}
+              loading="lazy"
+              alt={title}
+              src={img}
+              height={500}
+              width={500}
+            />
+            <Text fontStyle="italic">{subtitle}</Text>
+          </Box>
+          <Text fontWeight="bold">
+            {description.map((line, ind) => {
+              return (
+                <Fragment key={ind + "Description"}>
+                  {line}
+                  <br />
+                  <br />
+                </Fragment>
+              );
+            })}
+          </Text>
+        </Flex>
         <BackButton />
+        <CursoForm curso={title} />
       </Flex>
-      <Divider borderColor="gray.500" w="85%" m="auto" />
-      <Flex display="block" gap={2} p={3} flexDir="column">
-        <Box float="right" p={1}>
-          <Image
-            style={{
-              objectFit: "contain",
-              objectPosition: "center",
-              borderRadius: "10px",
-              border: "1px solid rgba(175, 175, 175, 0.4)",
-              padding: 1,
-            }}
-            loading="lazy"
-            alt={title}
-            src={img}
-            height={500}
-            width={500}
-          />
-          <Text fontStyle="italic">{subtitle}</Text>
-        </Box>
-        <Text fontWeight="bold">
-          {description.map((line, ind) => {
-            return (
-              <Fragment key={ind + "Description"}>
-                {line}
-                <br />
-                <br />
-              </Fragment>
-            );
-          })}
-        </Text>
-      </Flex>
-      <BackButton />
-      <CursoForm curso={title} />
-    </Flex>
+    </>
   );
 };
 
