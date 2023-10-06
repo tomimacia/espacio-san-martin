1532b91cf6b994326f6d305ae110a9c021c57b2d
Tomás Macía <tomimacia@gmail.com>
2023-10-06 14:07:57 -0300

    Noticias metadata

--- components/NoticiaStructure.tsx
+++ components/NoticiaStructure.tsx
@@ -9,6 +9,7 @@ import {
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
+import Head from "next/head";
 import Image from "next/image";
 import NextLink from "next/link";
 type CursoStr = {
@@ -26,67 +27,76 @@ const NoticiaStructure = ({
   img,
 }: CursoStr) => {
   return (
-    <Flex py={10} gap={5} w="100%" flexDir="column">
-      <Flex flexDir="column" align="center" justify="center" w="100%">
-        <Heading textAlign="center">{title}</Heading>
-        <Button
-          size="xs"
-          textDecor="underline"
-          as={NextLink}
-          _hover={{ color: "blue" }}
-          alignSelf="flex-end"
-          href="/"
-          bg="transparent"
-        >
-          <ArrowBackIcon />
-          Volver
-        </Button>
-      </Flex>
-      <Divider borderColor="gray.500" w="85%" m="auto" />
-      <Text textAlign="center" fontStyle="italic">
-        {subtitle}
-      </Text>
-      <Flex display="block" gap={2} p={3} flexDir="column">
-        <Box float="right" p={1}>
-          <Image
-            style={{
-              objectFit: "contain",
-              objectPosition: "center",
-              borderRadius: "10px",
-            }}
-            alt={imageFooter}
-            src={img}
-            height={500}
-            width={500}
-          />
-          <Text fontStyle="italic">{imageFooter}</Text>
-        </Box>
-        <Text fontWeight="bold">
-          {description.map((line) => {
-            return (
-              <>
-                {line}
-                <br />
-                <br />
-              </>
-            );
-          })}
+    <>
+      <Head>
+        <meta property="og:description" content={title} />
+        <meta
+          property="og:image"
+          content={`https://www.sanmartinjuancruz.com.ar/Cursos/${img}.jpeg`}
+        />
+      </Head>
+      <Flex py={10} gap={5} w="100%" flexDir="column">
+        <Flex flexDir="column" align="center" justify="center" w="100%">
+          <Heading textAlign="center">{title}</Heading>
+          <Button
+            size="xs"
+            textDecor="underline"
+            as={NextLink}
+            _hover={{ color: "blue" }}
+            alignSelf="flex-end"
+            href="/"
+            bg="transparent"
+          >
+            <ArrowBackIcon />
+            Volver
+          </Button>
+        </Flex>
+        <Divider borderColor="gray.500" w="85%" m="auto" />
+        <Text textAlign="center" fontStyle="italic">
+          {subtitle}
+        </Text>
+        <Flex display="block" gap={2} p={3} flexDir="column">
+          <Box float="right" p={1}>
+            <Image
+              style={{
+                objectFit: "contain",
+                objectPosition: "center",
+                borderRadius: "10px",
+              }}
+              alt={imageFooter}
+              src={img}
+              height={500}
+              width={500}
+            />
+            <Text fontStyle="italic">{imageFooter}</Text>
+          </Box>
+          <Text fontWeight="bold">
+            {description.map((line) => {
+              return (
+                <>
+                  {line}
+                  <br />
+                  <br />
+                </>
+              );
+            })}
+          </Text>
+        </Flex>
+        <Text fontStyle="italic" ml={3}>
+          Para más información{" "}
+          {
+            <Link
+              as={NextLink}
+              href="/Contacto"
+              color={useColorModeValue("blue", "blue.200")}
+              _hover={{ textDecor: "underline" }}
+            >
+              Contáctanos!
+            </Link>
+          }
         </Text>
       </Flex>
-      <Text fontStyle="italic" ml={3}>
-        Para más información{" "}
-        {
-          <Link
-            as={NextLink}
-            href="/Contacto"
-            color={useColorModeValue("blue", "blue.200")}
-            _hover={{ textDecor: "underline" }}
-          >
-            Contáctanos!
-          </Link>
-        }
-      </Text>
-    </Flex>
+    </>
   );
 };
 
